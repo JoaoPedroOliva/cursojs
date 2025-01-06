6658f9921371851cfb4a6777347f380bb682ba9c
João Pedro Severo Oliva <joaopedro.oliva10@gmail.com>
2025-01-06 06:50:54 -0300

    exercicio17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert("Olá");
+function tabuada() {
+  let numero = document.getElementById("tnum");
+  let tab = document.getElementById("seltab");
+  let valor = 0;
+  if (numero.value.length == 0) {
+    window.alert("Por favor, Digite um número");
+  } else {
+    numero = Number(numero.value);
+    tab.innerHTML = "";
+    for (let c = 1; c <= 10; c++) {
+      //5 * 1 = 5
+      let item = document.createElement("option");
+      item.text = `${numero} x ${c} = ${numero * c}`;
+      item.value = `tab${c}`;
+      tab.appendChild(item);
+    }
+  }
+}
